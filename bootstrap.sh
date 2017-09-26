199f17f81a9240d7c29f37872b6077d4fea99d83
Mark Talbot <mark.talbot@experiencepoint.com>
2017-09-26 16:20:53 -0400

    WIP more edits

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# function bootstrapTerminal() {  
+#     sudo -v #ask password beforehand
+#     source ~/.dotfiles/install.sh
+# }
+
+
+echo 'Bootstrap terminal'
+echo '------------------'
+echo 'This will reset your terminal. Are you sure you want to to this? (y/n) '
+read -p 'Answer: '  reply
+
+if [[ $reply =~ ^[Yy]$ ]]
+then
+   # bootstrapTerminal
+   echo 'hiiii'
+fi   
